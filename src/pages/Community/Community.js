f1cb05a1dd9fb73f03970512194209d48efadcca
melnicenkovadik <melnicenkovadik@gmail.com>
2022-03-21 19:08:29 +0100

    init typescript

--- src/pages/Community/Community.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-const Community = () => <h1>Community</h1>;
-
-export default Community;
--- src/pages/Developers/Developers.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-const Developers = () => <h1>Developers</h1>;
-
-export default Developers;
