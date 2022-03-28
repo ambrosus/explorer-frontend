4e130e1fff10cfb7c4da0962aee1e52e83afe3f0
Georg Grigoryan <antikiok@gmail.com>
2022-03-29 01:10:12 +0200

    end lyaout home

--- src/components/LastestTransactions/index.tsx
+++ src/components/LastestTransactions/index.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GreenCircle from '../../assets/icons/GreenCircle';
+import OrangeCircle from '../../assets/icons/OrangeCircle';
 
 const LastestTransactions = () => {
+	const online: boolean = true;
 	return (
-		<>
-			<div>LastestTransactions</div>
-		</>
+		<tbody className='lastestTransactions__tbody'>
+			<tr className='lastestTransactions__tr'>
+				<td className='lastestTransactions__td'>
+					<p className='lastestTransactions__p '>
+						<span className='lastestTransactions__circle'>{online ? <GreenCircle /> : <OrangeCircle />}</span>
+						0x9012...328eb
+					</p>
+
+					<p className='lastestTransactions__p'>
+						<span className='lastestTransactions__span-light'>6 secs ago</span>
+					</p>
+				</td>
+				<td className='lastestTransactions__td'>
+					<div className='lastestTransactions__td-div'>
+						<p className='lastestTransactions__p'>
+							<span className='lastestTransactions__span-light'>From</span>
+							<span className='lastestTransactions__span-dark lastestTransactions__margin-left'>MiningPoolHub</span>
+						</p>
+						<p className='lastestTransactions__p'>
+							<span className='lastestTransactions__span-dark'>To</span>
+							<span className='lastestTransactions__span-light lastestTransactions__margin-left'>46 secs ago</span>
+						</p>
+					</div>
+				</td>
+				<td className='lastestTransactions__td'>
+					<div className='lastestTransactions__td-div'>
+						<p className='lastestTransactions__p' style={{ textAlign: 'right' }}>
+							<span className='lastestTransactions__span-light'>Amount</span>
+						</p>
+						<p className='lastestTransactions__p'>
+							<span className='lastestTransactions__span-dark'>1.71139 AMB</span>
+						</p>
+					</div>
+				</td>
+			</tr>
+		</tbody>
 	);
 };
 
--- src/components/LatestBlocks/index.tsx
+++ src/components/LatestBlocks/index.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GreenCircle from '../../assets/icons/GreenCircle';
+import OrangeCircle from '../../assets/icons/OrangeCircle';
 
 const LatestBlocks = () => {
+	const online: boolean = true;
 	return (
-		<>
-			<div>LatestBlocks</div>
-		</>
+		<tbody className='latestBlocks__tbody'>
+			<tr className='latestBlocks__tr'>
+				<td className='latestBlocks__td'>
+					<p className='latestBlocks__p '>
+						<span className='latestBlocks__circle'>{online ? <GreenCircle /> : <OrangeCircle />}</span>
+						17122153
+					</p>
+
+					<p className='latestBlocks__p'>
+						<span className='latestBlocks__span-light'>46 secs ago</span>
+					</p>
+				</td>
+				<td className='latestBlocks__td'>
+					<div className='latestBlocks__td-div'>
+						<p className='latestBlocks__p'>
+							<span className='latestBlocks__span-light'>Miner</span>
+							<span className='latestBlocks__span-dark latestBlocks__margin-left'>MiningPoolHub</span>
+						</p>
+						<p className='latestBlocks__p'>
+							<span className='latestBlocks__span-dark'>0 txns</span>
+							<span className='latestBlocks__span-light latestBlocks__margin-left'>46 secs ago</span>
+						</p>
+					</div>
+				</td>
+				<td className='latestBlocks__td'>
+					<div className='latestBlocks__td-div'>
+						<p className='latestBlocks__p'>
+							<span className='latestBlocks__span-light'>Block Reward</span>
+						</p>
+						<p className='latestBlocks__p'>
+							<span className='latestBlocks__span-dark'>1.71139 AMB</span>
+						</p>
+					</div>
+				</td>
+			</tr>
+		</tbody>
 	);
 };
 
