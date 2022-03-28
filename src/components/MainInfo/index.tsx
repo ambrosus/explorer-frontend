4db7149c152b3a29f4458756202ddc24d4b5b154
Georg Grigoryan <antikiok@gmail.com>
2022-03-28 02:17:28 +0200

    layout home body

--- src/components/MainInfo/index.tsx
+++ src/components/MainInfo/index.tsx
@@ -4,13 +4,13 @@ import MarketCap from '../../assets/icons/MarketCap';
 const MainInfo = () => {
 	return (
 		<>
-			<td className='tableInfo__td'>
-				<div className='tableInfo__icon'>
+			<td className='mainInfo__td'>
+				<div className='mainInfo__icon'>
 					<MarketCap />
 				</div>
 				<div>
-					<p className='tableInfo__p'>MARKET CAP</p>
-					<p className='tableInfo__p'>17,895,744.27 USD</p>
+					<p className='mainInfo__p'>MARKET CAP</p>
+					<p className='mainInfo__p'>17,895,744.27 USD</p>
 				</div>
 			</td>
 		</>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,12 @@ import API from '../../API/api';
 import storage from '../../utils/storage';
 import FindWide from '../../components/FindWide';
 import MainInfo from '../../components/MainInfo';
+import LastestsInfo from '../../components/LastestsInfo/LastestsInfo';
 
 export const Home = () => {
+	const viewBlocks = () => console.log('viewBlocks');
+	const viewTransactions = () => console.log('viewTransactions');
+
 	const getHomePageData = async () => {
 		const latestBlocks = await API.getBlocks({ limit: 8 });
 		const latestTransactions = await API.getTransactions({ limit: 8 });
@@ -33,14 +37,14 @@ export const Home = () => {
 				<Content.Header>
 					<h1 className='home__h1'>Ambrosus Network Explorer</h1>
 					<FindWide />
-					<table className='tableInfo'>
-						<tbody className='tableInfo__tbody'>
-							<tr className='tableInfo__tr'>
+					<table className='mainInfo'>
+						<tbody className='mainInfo__tbody'>
+							<tr className='mainInfo__tr'>
 								<MainInfo />
 								<MainInfo />
 								<MainInfo />
 							</tr>
-							<tr className='tableInfo__tr'>
+							<tr className='mainInfo__tr'>
 								<MainInfo />
 								<MainInfo />
 								<MainInfo />
@@ -49,7 +53,56 @@ export const Home = () => {
 					</table>
 				</Content.Header>
 				<Content.Body>
-					<div className='container'>Lastest Blocks</div>
+					<section className='home__tables'>
+						<div className='home__content'>
+							<table className='lastestsInfo'>
+								<thead>
+									<tr>
+										<td className='home__h2'>Lastest Blocks</td>
+									</tr>
+								</thead>
+								<tbody className='lastestsInfo__tbody'>
+									<tr className='lastestsInfo__tr'>
+										<LastestsInfo />
+										<LastestsInfo />
+										<LastestsInfo />
+									</tr>
+									<tr className='lastestsInfo__tr'>
+										<LastestsInfo />
+										<LastestsInfo />
+										<LastestsInfo />
+									</tr>
+								</tbody>
+							</table>
+							<button className='home__btn' onClick={viewBlocks}>
+								View all blocks
+							</button>
+						</div>
+						<div className='home__content'>
+							<table className='lastestsInfo'>
+								<thead>
+									<tr>
+										<td className='home__h2'>Lastest Transactions</td>
+									</tr>
+								</thead>
+								<tbody className='lastestsInfo__tbody'>
+									<tr className='lastestsInfo__tr'>
+										<LastestsInfo />
+										<LastestsInfo />
+										<LastestsInfo />
+									</tr>
+									<tr className='lastestsInfo__tr'>
+										<LastestsInfo />
+										<LastestsInfo />
+										<LastestsInfo />
+									</tr>
+								</tbody>
+							</table>
+							<button className='home__btn' onClick={viewTransactions}>
+								View all transactions
+							</button>
+						</div>
+					</section>
 				</Content.Body>
 			</section>
 		</Content>
