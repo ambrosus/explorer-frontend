7d9f83c8274cae53426ecefefc0b1a5f9264adf3
melnicenkovadik <melnicenkovadik@gmail.com>
2022-04-02 11:25:10 +0200

    render transaction

---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import AddressBlocksHeader from '../AddressBlocksHeader';
+import AddressBlock from '../AddressBlocks';
+import ViewMoreBtn from '../ViewMoreBtn';
+import { formatEther } from 'ethers/lib/utils';
+//create functional component Tabs without store  with the following tabs: 'All','Transfers','ERC-20 Tx','Block
+// Rewards' following the pattern of the Tabs component in the UI. The Tabs component is used to display the
+// different tabs in the UI. need to add the following props:"transfers","block_rewards" ," ERC-20_Tx"  props will
+// come from API call to get the data for the tabs. The Tabs component will render the tabs based on the	props
+// passed to it.
+
+const transactionFilters = [
+	{ title: 'All', value: '' },
+	{ title: 'Transfers', value: 'transfers' },
+	{ title: 'Block Rewards', value: 'block_rewards' },
+	{ title: 'ERC-20 Tx', value: 'ERC-20_Tx' },
+];
+
+const Tabs = ({ data, setTransactionType }:any) => {
+	const { address } = useParams();
+
+	return	(
+		<>
+		<div>
+			{transactionFilters.map(filter => (
+				<Link
+					key={filter.title}
+					to={`/addresses/${address}/${filter.value}`}
+					className="tabs__link"
+					onClick={() => setTransactionType(filter.value)}
+				>
+					{filter.title}
+				</Link>
+			))}
+			<div>
+				<section className='addressDetails__table'>
+					<AddressBlocksHeader
+						txhash='txHash'
+						method='Method'
+						from='From'
+						to='To'
+						date='Date'
+						block='Block'
+						amount='Amount'
+						txfee='txFee'
+					/>
+					{data?.transactions.map((transaction:any, index:number) => {
+						return (
+							<AddressBlock
+								key={transaction.hash}
+								txhash={`${transaction.hash.slice(0, 10)}...${transaction.hash.slice(
+									transaction.hash.length - 10,
+								)}`}
+								method={transaction.type}
+								from={`${transaction.from.slice(0, 5)}...${transaction.from.slice(
+									transaction.from.length - 5,
+								)}`}
+								to={`${transaction.to.slice(0, 5)}...${transaction.to.slice(
+									transaction.to.length - 5,
+								)}`}
+								date='1 min ago'
+								block='10986508'
+								amount={`${Number(formatEther(transaction.value.wei)).toFixed(2)} ETH`}
+								txfee='0.000105 AMB'
+							/>
+						);
+					})}
+				</section>
+				<div style={{ marginTop: '10px', display: 'flex', alignItems: 'center' }}>
+					<ViewMoreBtn nameBtn='Load More' />
+				</div>
+
+			</div>
+		</div>
+		</>
+	)
+}
+export default Tabs;
--- src/pages/Addresses/AddressDetails/index.tsx
+++ src/pages/Addresses/AddressDetails/index.tsx
@@ -4,29 +4,18 @@ import { useParams } from 'react-router-dom';
 import ContentCopy from '../../../assets/icons/ContentCopy';
 import API from '../../../API/api';
 import OveralBalance from '../../../components/OveralBalance';
-import AddressBlock from '../../../components/AddressBlocks';
-import AddressBlocksHeader from '../../../components/AddressBlocksHeader';
-import ViewMoreBtn from '../../../components/ViewMoreBtn';
 
 import Token from '../../../components/Token';
-// import { formatEther } from 'ethers/lib/utils';
+import Tabs from '../../../components/Tabs';
 
-const transactionFilters = [
-	{ title: 'All', value: '' },
-	{ title: 'Transfers', value: 'transfers' },
-	{ title: 'Block Rewards', value: 'block_rewards' },
-	{ title: 'ERC-20 Tx', value: 'ERC-20_Tx' },
-];
 
 export const AddressDetails = () => {
 	const { address } = useParams();
 	const [transactionType, setTransactionType] = useState('');
 	const [addressData, setAddressData] = useState<any>(null);
+const sybStringAddress = `${address && address.slice(0, 10)}...${address && address.slice(address.length - 10,)}`;
 
-	// const { ethereum }: any = window;
-
-	const reciveAdress = '0xF977814e90dA44bFA03b6295A0616a897441aceC';
-	const copyConten = () => console.log(reciveAdress);
+	const copyConten = () => console.log(sybStringAddress);
 
 
 	useEffect(() => {
@@ -41,7 +30,7 @@ export const AddressDetails = () => {
 			<section className='addressDetails'>
 				<Content.Header>
 					<h1 className='addressDetails__h1'>
-						{address} <span className='addressDetails__h1-span'>{reciveAdress}</span>
+						Address Details	 <span className='addressDetails__h1-span'>	{sybStringAddress}</span>
 						<button className='addressDetails__h1-btn' onClick={copyConten}>
 							<ContentCopy />
 						</button>
@@ -54,50 +43,7 @@ export const AddressDetails = () => {
 					</div>
 				</Content.Header>
 				<Content.Body>
-					<section>
-						{transactionFilters.map(({ value, title }) => (
-							<button key={value} onClick={() => setTransactionType(value.toLowerCase())}>
-								{title}
-							</button>
-						))}
-					</section>
-
-					<section className='addressDetails__table'>
-						<AddressBlocksHeader
-							txhash='txHash'
-							method='Method'
-							from='From'
-							to='To'
-							date='Date'
-							block='Block'
-							amount='Amount'
-							txfee='txFee'
-						/>
-
-						<AddressBlock
-							txhash='0xfad804b6f81b...6aa121c5485b'
-							method='Transfer'
-							from='0x9012...328eb'
-							to='0x9012...328eb'
-							date='1 min ago'
-							block='10986508'
-							amount='1.33345 ETH'
-							txfee='Pending'
-						/>
-						<AddressBlock
-							txhash='0xfad804b6f81b...6aa121c5485b'
-							method='Transfer'
-							from='0x9012...328eb'
-							to='0x9012...328eb'
-							date='1 min ago'
-							block='10986508'
-							amount='1.33345 ETH'
-							txfee='0.000105 AMB'
-						/>
-					</section>
-					<div style={{ marginTop: '10px', display: 'flex', alignItems: 'center' }}>
-						<ViewMoreBtn nameBtn='Load More' />
-					</div>
+					<Tabs data={addressData} setTransactionType={setTransactionType}/>
 				</Content.Body>
 			</section>
 		</Content>
