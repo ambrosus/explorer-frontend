c959d6af8078dc41dcb588d27f5cefb8691037e2
Georg Grigoryan <antikiok@gmail.com>
2022-05-02 09:12:01 +0200

    fix layout and helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,29 +1,53 @@
-import moment from "moment";
+import moment from 'moment'
 
-export const sliceData5 = (item: string| any) => {
-  if (!item) {
-    return "";
-  }
-  return item.length > 5 ? `${item.slice(0, 5)}...${item.slice(item.length - 5)}` : item;
-};
-export const sliceData10 = (item: string| any) => {
-  if (!item) {
-    return "";
-  }
-  return item.length > 10 ? `${item.slice(0, 10)}...${item.slice(item.length - 10)}` : item;
-};
-export const calcTime = (time: any) => moment(time).isValid() ? moment(time * 1000).fromNow() : "";
+export const sliceData5 = (item: string | any) => {
+	if (!item) {
+		return ''
+	}
+	return item.length > 5
+		? `${item.slice(0, 5)}...${item.slice(item.length - 5)}`
+		: item
+}
+export const sliceData10 = (item: string | any) => {
+	if (!item) {
+		return ''
+	}
+	return item.length > 10
+		? `${item.slice(0, 10)}...${item.slice(item.length - 10)}`
+		: item
+}
+export const calcTime = (time: any) =>
+	moment(time).isValid() ? moment(time * 1000).fromNow() : ''
 
-export const setActiveLink = ((props: {
-  isActive: boolean;
-}): string | undefined => "tabs__link " + (props.isActive ? "tabs__link-active" : ""));
+export const setActiveLink = (props: {
+	isActive: boolean
+}): string | undefined =>
+	'tabs__link ' + (props.isActive ? 'tabs__link-active' : '')
 
-export const copyContent: (copyObj: any, setIsCopy: Function) => void = (copyObj: any, setIsCopy: Function) => {
-  if (copyObj ){
-    navigator.clipboard.writeText(copyObj);
-    setIsCopy(true);
-    setTimeout(() => setIsCopy(false), 1000);
-  } else {
-    setIsCopy(false);
-  }
-};
+export const copyContent: (copyObj: any, setIsCopy: Function) => void = (
+	copyObj: any,
+	setIsCopy: Function
+) => {
+	if (copyObj) {
+		navigator.clipboard.writeText(copyObj)
+		setIsCopy(true)
+		setTimeout(() => setIsCopy(false), 1000)
+	} else {
+		setIsCopy(false)
+	}
+}
+
+export const setupStyle = (item: string | undefined) => {
+	if (item) {
+		let type: { style: object } = {
+			style: {},
+		}
+		switch (item) {
+			case 'ERC-20_Tx':
+				return (type.style = { gridTemplateColumns: 'repeat(7, auto)' })
+
+			default:
+				return (type.style = { gridTemplateColumns: 'repeat(8, auto)' })
+		}
+	}
+}
