d696b7e9fad414212d8317dc292f825e16cb0174
Georg Grigoryan <antikiok@gmail.com>
2022-05-09 23:43:40 +0200

    layout address

--- src/pages/Addresses/index.tsx
+++ src/pages/Addresses/index.tsx
@@ -19,32 +19,32 @@ export const Addresses = () => (
 			</div>
 		</Content.Header>
 		<Content.Body>
-			<div className="addresses__table">
-				<div className="addressesSort__table">
-					<div className="addressesSort__heading">Addresses</div>
-					<div className="addressesSort__cells">
-						<div className="addressesSort__cell">Sort by</div>
-						<button className="addressesSort__cell">Addresses</button>
-						<button className="addressesSort__cell">Balance</button>
-						<button className="addressesSort__cell">Tx Count</button>
+			<div className="addresses__mainTable">
+				<div className="addresses__sort">
+					<div className="addresses__sort-heading">Addresses</div>
+					<div className="addresses__sort-cells">
+						<div className="addresses__sort-cell">Sort by</div>
+						<button className="addresses__sort-cell">Addresses</button>
+						<button className="addresses__sort-cell">Balance</button>
+						<button className="addresses__sort-cell">Tx Count</button>
 					</div>
 				</div>
-				<div className="addressesTable">
-					<div className="addressesHeader__cells">
-						<div className="addressesHeader__cell">Rank</div>
-						<div className="addressesHeader__cell">Address</div>
-						<div className="addressesHeader__cell">Tx Count</div>
-						<div className="addressesHeader__cell">Balance</div>
-						<div className="addressesHeader__cell">Holding</div>
+				<div className="addresses__table">
+					<div className="addresses__header">
+						<div className="addresses__header-cell">Rank</div>
+						<div className="addresses__header-cell">Address</div>
+						<div className="addresses__header-cell">Tx Count</div>
+						<div className="addresses__header-cell">Balance</div>
+						<div className="addresses__header-cell">Holding</div>
 					</div>
-					<div className="addressesBody__cells">
-						<div className="addressesBody__cell">1</div>
-						<div className="addressesBody__cell">
+					<div className="addresses__body">
+						<div className="addresses__body-cell">1</div>
+						<div className="addresses__body-cell">
 							0xF977814e90dA44bFA03b6295A0616a897441aceC
 						</div>
-						<div className="addressesBody__cell">550</div>
-						<div className="addressesBody__cell">1.33345 AMB</div>
-						<div className="addressesBody__cell">0.00%</div>
+						<div className="addresses__body-cell">550</div>
+						<div className="addresses__body-cell">1.33345 AMB</div>
+						<div className="addresses__body-cell">0.00%</div>
 					</div>
 				</div>
 			</div>
