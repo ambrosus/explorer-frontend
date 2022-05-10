ffc32ff00d824f9b132c0093915241d2eaba9dc1
Georg Grigoryan <antikiok@gmail.com>
2022-05-10 21:54:22 +0200

    AMB 606 task2 fix

---
+++ src/components/AddressesSort/index.tsx
@@ -0,0 +1,15 @@
+import react from 'react'
+
+const AddressesSort = () => (
+	<div className="addresses__sort">
+		<div className="addresses__sort-heading">Addresses</div>
+		<div className="addresses__sort-cells">
+			<div className="addresses__sort-cell">Sort by</div>
+			<button className="addresses__sort-cell">Addresses</button>
+			<button className="addresses__sort-cell">Balance</button>
+			<button className="addresses__sort-cell">Tx Count</button>
+		</div>
+	</div>
+)
+
+export default AddressesSort
