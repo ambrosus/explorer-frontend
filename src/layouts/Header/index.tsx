ee31a7dbdfa2e871b93963c535a5fe974b7bca11
Georg Grigoryan <antikiok@gmail.com>
2022-05-11 11:28:15 +0200

    fix task

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,7 +1,6 @@
 import DesctopMenu from 'components/DesctopMenu'
 import MobileMenu from 'components/MobileMenu'
 import useWindowSize from 'hooks/useWindowSize'
-import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { routes as menuItems } from 'routes'
 import { IRoute } from 'types'
@@ -11,23 +10,27 @@ import AmbrosusLogoSvg from './AmbrosusLogoSvg'
 export const Header = () => {
 	const { width } = useWindowSize()
 
+	const isMobileStyle = width > 1100 ? 'menu__item' : 'menuMobile__item'
+
 	const menu = menuItems.map((menuElement: IRoute) => {
+		const cursor = menuElement.isClick ? 'universall__hover' : ''
 		const activeStyle = {
 			color: '#fff',
+			cursor: cursor,
 		}
-
 		const disableClick = (e: any) => {
 			e.preventDefault()
 		}
+
 		return (
 			<NavLink
 				to={menuElement.path}
 				key={menuElement.key}
-				className={width > 1100 ? 'menu__item' : 'menuMobile__item'}
+				className={`${isMobileStyle} ${cursor}`}
 				style={({ isActive }) => ({
 					...(isActive ? activeStyle : null),
 				})}
-				onClick={(e) => disableClick(e)}
+				onClick={(e) => !menuElement.isClick && disableClick(e)}
 			>
 				{menuElement.key}
 			</NavLink>
