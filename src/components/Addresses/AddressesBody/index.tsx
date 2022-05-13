68c1ccf4052d825672d60fb4ae96b6260ec3e4a7
melnicenkovadik <melnicenkovadik@gmail.com>
2022-05-13 16:45:15 +0200

    add accounts page

--- src/components/Addresses/AddressesBody/index.tsx
+++ src/components/Addresses/AddressesBody/index.tsx
@@ -1,13 +1,33 @@
-const AddressesBody = () => (
-	<div className="addresses__body">
-		<div className="addresses__body-cell">1</div>
-		<div className="addresses__body-cell">
-			0xF977814e90dA44bFA03b6295A0616a897441aceC
-		</div>
-		<div className="addresses__body-cell">550</div>
-		<div className="addresses__body-cell">1.33345 AMB</div>
-		<div className="addresses__body-cell">0.00%</div>
-	</div>
-)
+import { useTypedSelector } from '../../../hooks/useTypedSelector'
+
+const AddressesBody = ({
+	address,
+	balance,
+	rank,
+	txCount,
+	lastCardRef,
+}: any) => {
+	const { data: appData } = useTypedSelector((state: any) => state.app)
+	const totalSupply = appData && appData.tokenInfo.total_supply
+
+	const ambBalance = balance && balance.ether ? balance.ether : 0
+
+	const holdingPercentage = (ambBalance / totalSupply) * 100
+
+	return (
+		appData && address && (
+			<div className="addresses__body" ref={lastCardRef}>
+				<div className="addresses__body-cell">{rank}</div>
+				<div className="addresses__body-cell">{address}</div>
+				<div className="addresses__body-cell">{txCount}</div>
+				<div className="addresses__body-cell">{ambBalance.toFixed(2)} AMB</div>
+
+				<div className="addresses__body-cell">
+					{(holdingPercentage || 0).toFixed(2)} %
+				</div>
+			</div>
+		)
+	)
+}
 
 export default AddressesBody
