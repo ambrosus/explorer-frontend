49808928e8373652e1e90455738b50cd3e27146c
Georg Grigoryan <antikiok@gmail.com>
2022-05-19 16:52:35 +0200

    fix copyed

--- src/components/Addresses/AddressesHeader/index.tsx
+++ src/components/Addresses/AddressesHeader/index.tsx
@@ -1,11 +1,11 @@
 const AddressesHeader = () => (
-  <div className="addresses__header">
+  <>
     <div className="addresses__header-cell">Rank</div>
     <div className="addresses__header-cell">Address</div>
     <div className="addresses__header-cell">Tx Count</div>
     <div className="addresses__header-cell">Balance</div>
     <div className="addresses__header-cell">Holding</div>
-  </div>
+  </>
 );
 
 export default AddressesHeader;
