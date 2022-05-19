84ec48ad2d61bfa03458213b19d9bf216fe40e21
Georg Grigoryan <antikiok@gmail.com>
2022-05-20 00:06:20 +0200

    fix table layout

--- src/components/OveralBalance/index.tsx
+++ src/components/OveralBalance/index.tsx
@@ -1,13 +1,15 @@
+import { TParams } from '../../types';
 import { useTypedSelector } from 'hooks/useTypedSelector';
 import { OverallBalanceProps } from 'pages/Addresses/AddressDetails/address-details.interface';
-import React, { useCallback, useEffect, useState } from 'react'
-import { TParams } from '../../types'
-import { useParams } from 'react-router-dom'
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const OverallBalance: React.FC<OverallBalanceProps> = ({
   addressBalance = 0,
 }) => {
-  const {loading, data: appData } = useTypedSelector((state: any) => state.app);
+  const { loading, data: appData } = useTypedSelector(
+    (state: any) => state.app,
+  );
   const { address }: TParams = useParams();
   const [balance, setBalance] = useState<any>(addressBalance);
   const [amountInUsd, setAmountInUsd] = useState(0);
@@ -19,11 +21,11 @@ const OverallBalance: React.FC<OverallBalanceProps> = ({
     if (appData && appData?.total_price_usd && appData.total_price_usd) {
       setAmountInUsd(appData.total_price_usd * Number(addressBalance));
     }
-  },[balance,addressBalance ,amountInUsd,address])
+  }, [balance, addressBalance, amountInUsd, address]);
 
   useEffect(() => {
-    setValues()
-  }, [address,appData,addressBalance,balance, loading,amountInUsd]);
+    setValues();
+  }, [address, appData, addressBalance, balance, loading, amountInUsd]);
 
   return (
     <div className="addressDetails__div">
