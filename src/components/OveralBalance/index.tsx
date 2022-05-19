dbe262d18a896bf78897fd15af4ec2475ad743d4
melnicenkovadik <melnicenkovadik@gmail.com>
2022-05-19 18:32:52 +0200

    updates balance

--- src/components/OveralBalance/index.tsx
+++ src/components/OveralBalance/index.tsx
@@ -1,47 +1,56 @@
-import { useTypedSelector } from 'hooks/useTypedSelector';
-import { OverallBalanceProps } from 'pages/Addresses/AddressDetails/address-details.interface';
-import React, { useCallback, useEffect, useState } from 'react'
 import { TParams } from '../../types'
+import { useTypedSelector } from 'hooks/useTypedSelector'
+import { OverallBalanceProps } from 'pages/Addresses/AddressDetails/address-details.interface'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useDebounce } from '../../hooks/useDebounce'
 
 const OverallBalance: React.FC<OverallBalanceProps> = ({
-  addressBalance = 0,
-}) => {
-  const {loading, data: appData } = useTypedSelector((state: any) => state.app);
-  const { address }: TParams = useParams();
-  const [balance, setBalance] = useState<any>(addressBalance);
-  const [amountInUsd, setAmountInUsd] = useState(0);
+																												 addressBalance = 0,
+																											 }) => {
+	const { loading, data: appData } = useTypedSelector(
+		(state: any) => state.app,
+	)
+	const { address }: TParams = useParams()
+	const [balance, setBalance] = useState<any>(Number(addressBalance))
+	const [amountInUsd, setAmountInUsd] = useState<any>(Number(addressBalance))
 
-  const setValues = useCallback(() => {
-    if (addressBalance) {
-      setBalance(addressBalance);
-    }
-    if (appData && appData?.total_price_usd && appData.total_price_usd) {
-      setAmountInUsd(appData.total_price_usd * Number(addressBalance));
-    }
-  },[balance,addressBalance ,amountInUsd,address])
 
-  useEffect(() => {
-    setValues()
-  }, [address,appData,addressBalance,balance, loading,amountInUsd]);
+	const balMemo = useMemo(() => balance ? Number(addressBalance) : Number(addressBalance), [address, addressBalance])
+	let amountInUsdMemo = useMemo(() => {
+		return appData && appData?.total_price_usd && appData.total_price_usd && appData.total_price_usd * Number(addressBalance) || 0
+	}, [appData, address])
 
-  return (
-    <div className="addressDetails__div">
+	const deboucePrice = useDebounce(amountInUsdMemo, 1000)
+	useEffect(() => {
+		if (!balMemo || address) {
+			if (addressBalance) {
+				setBalance(addressBalance)
+			}
+			if (appData && appData?.total_price_usd && appData.total_price_usd) {
+				setAmountInUsd(appData.total_price_usd * Number(addressBalance))
+			}
+		}
+	}, [addressBalance, address, amountInUsd])
+
+
+	return (
+		<div className='addressDetails__div'>
       <span
-        className="addressDetails__div-span universall__dark"
-        style={{ fontWeight: 700 }}
-      >
+				className='addressDetails__div-span universall__dark'
+				style={{ fontWeight: 700 }}
+			>
         Balance
       </span>
-      <span className="addressDetails__div-span universall__dark">
-        {`${balance} AMB`}{' '}
+			<span className='addressDetails__div-span universall__dark'>
+        {`${balMemo ? Number(balMemo).toFixed(2) : Number(balance).toFixed(2)} AMB`}{' '}
       </span>
-      <span className="addressDetails__div-span universall__dark">/</span>
-      <span className="addressDetails__div-span universall__light2">{`$ ${
-        amountInUsd && amountInUsd.toFixed(2)
-      }`}</span>
-    </div>
-  );
-};
+			<span className='addressDetails__div-span universall__dark'>/</span>
+			<span className='addressDetails__div-span universall__light2'>{`$ ${
+				amountInUsd ? amountInUsd.toFixed(2) : deboucePrice.toFixed(2)
+			}`}</span>
+		</div>
+	)
+}
 
-export default OverallBalance;
+export default OverallBalance
