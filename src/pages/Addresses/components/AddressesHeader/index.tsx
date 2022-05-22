20fdb177bf6b0cb4b45357d773e39faa73f6d871
Georg Grigoryan <antikiok@gmail.com>
2022-05-22 11:45:48 +0200

    codestyle refactoring

--- src/pages/Addresses/components/AddressesHeader/index.tsx
+++ src/pages/Addresses/components/AddressesHeader/index.tsx
@@ -1,10 +1,10 @@
 const AddressesHeader = () => (
   <>
-    <div className="addresses__header-cell">Rank</div>
-    <div className="addresses__header-cell">Address</div>
-    <div className="addresses__header-cell">Tx Count</div>
-    <div className="addresses__header-cell">Balance</div>
-    <div className="addresses__header-cell">Holding</div>
+    <div className="addresses_header_cell">Rank</div>
+    <div className="addresses_header_cell">Address</div>
+    <div className="addresses_header_cell">Tx Count</div>
+    <div className="addresses_header_cell">Balance</div>
+    <div className="addresses_header_cell">Holding</div>
   </>
 );
 
--- src/pages/Addresses/components/AddressesSort/index.tsx
+++ src/pages/Addresses/components/AddressesSort/index.tsx
@@ -5,15 +5,15 @@ import {
 import { FC } from 'react';
 
 const AddressesSort: FC<PAddressesSort> = ({ sortTerm, setSortTerm }) => (
-  <div className="addresses__sort">
-    <div className="addresses__sort-heading">Addresses</div>
-    <div className="addresses__sort-cells">
-      <div className="addresses__sort-cell">Sort by</div>
+  <div className="addresses_sort">
+    <div className="addresses_sort_heading">Addresses</div>
+    <div className="addresses_sort_cells">
+      <div className="addresses_sort_cell">Sort by</div>
       {sortOptions.map((option, index) => (
         <div
           key={index}
-          className={`addresses__sort-cell pointer ${
-            option.value === sortTerm && 'addresses__sort-active'
+          className={`addresses_sort_cell pointer ${
+            option.value === sortTerm && 'addresses_sort_active'
           }`}
           onClick={() => {
             setSortTerm(option.value);
