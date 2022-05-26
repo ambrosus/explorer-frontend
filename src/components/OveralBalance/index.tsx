a80ad3bb67736d486aa8e278cd3a27a4a7e3aebe
dimavoronin <zveruha1109>
2022-05-26 12:11:33 +0300

    fix for balance

--- src/components/OveralBalance/index.tsx
+++ src/components/OveralBalance/index.tsx
@@ -1,25 +1,39 @@
-import { useTypedSelector } from 'hooks/useTypedSelector'
-import { OverallBalanceProps } from 'pages/Addresses/AddressDetails/address-details.interface'
-import React, { useEffect, useState } from 'react'
+import { useTypedSelector } from 'hooks/useTypedSelector';
+import { OverallBalanceProps } from 'pages/Addresses/AddressDetails/address-details.interface';
+import React, { useEffect, useState } from 'react';
 
-const OverallBalance: React.FC<OverallBalanceProps> = ({ addressBalance=0 }) => {
-  const [amountInUsd, setAmountInUsd] = useState(0)
-  const { data: appData } = useTypedSelector((state: any) => state.app)
+let amountInUsdBuffer = 0;
 
-  useEffect( () => {
-    if (appData && appData?.total_price_usd && appData.total_price_usd) {
-      setAmountInUsd(appData.total_price_usd * Number(addressBalance))
+const OverallBalance: React.FC<OverallBalanceProps> = ({
+  addressBalance,
+}) => {
+  const [amountInUsd, setAmountInUsd] = useState(amountInUsdBuffer);
+  const [addressBalanceBuffer, setAddressBalanceBuffer] = useState(+addressBalance);
+  const { data: appData } = useTypedSelector((state: any) => state.app);
+
+  useEffect(() => {
+    console.log(addressBalance);
+    if (
+      addressBalance !== undefined &&
+      appData?.total_price_usd
+    ) {
+
+      const usdPrice = appData.total_price_usd * +addressBalance;
+      amountInUsdBuffer = usdPrice
+      setAmountInUsd(usdPrice);
+
+      setAddressBalanceBuffer(+addressBalance)
     }
-  }, [addressBalance])
+  }, [addressBalance]);
 
   return (
     <div className="address_details_info_text">
-			<span
+      <span
         className="address_details_info_text_span universall_dark"
         style={{ fontWeight: 700, marginRight: 8 }}
       >
-				Balance
-			</span>
+        Balance
+      </span>
       <div
         style={{
           display: 'flex',
@@ -27,17 +41,17 @@ const OverallBalance: React.FC<OverallBalanceProps> = ({ addressBalance=0 }) => 
         }}
       >
         <span className="address_details_info_text_span universall_dark">
-				{`${addressBalance} AMB`}{' '}
-			</span>
+          {`${addressBalanceBuffer} AMB`}{' '}
+        </span>
         <span className="address_details_info_text_span universall_dark">
           /
         </span>
         <span className="address_details_info_text_span universall_light2">{`$ ${
-        amountInUsd && amountInUsd.toFixed(2)
-      }`}</span>
+          amountInUsd && amountInUsd.toFixed(2)
+        }`}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OverallBalance
+export default OverallBalance;
