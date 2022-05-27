17a65d106d880c0c75ef862b58b87819291ca6dc
Georg Grigoryan <antikiok@gmail.com>
2022-05-27 03:03:38 +0200

    fix task 5

--- src/pages/Home/components/LastestTransactions/index.tsx
+++ src/pages/Home/components/LastestTransactions/index.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { LatestTransactionsProps } from 'pages/Home/home.interfaces';
 import React from 'react';
 import { calcTime, isOnline, sliceData5, wrapString } from 'utils/helpers';
@@ -10,48 +11,54 @@ const LatestTransactions: React.FC<LatestTransactionsProps> = ({
   to,
   amount,
   type,
-}) => (
-  <>
-    <div className="lastest_transactions_cells">
-      <div className="lastest_transactions_cell">
-        <div className="lastest_transactions_cell_content lastest_transactions_font_big">
-          <span style={{ marginRight: 8 }}>{isOnline(status)}</span>
-          {sliceData5(hash)}
-        </div>
+}) => {
+  return (
+    <>
+      <div className="lastest_transactions_cells">
+        <div className="lastest_transactions_cell">
+          <div className="lastest_transactions_cell_content lastest_transactions_font_big">
+            <span style={{ marginRight: 8 }}>{isOnline(status)}</span>
+            {sliceData5(hash)}
+          </div>
 
-        <div className="lastest_transactions_p lastest_transactions_font_small">
-          <span style={{ marginRight: 16 }}></span>
-          {calcTime(timestamp)}
-        </div>
-      </div>
-
-      <div className="lastest_transactions_cell">
-        <div className="lastest_transactions_cell_content">
-          <div className="lastest_transactions_font_small">From</div>
-          <div className="lastest_transactions_font_big lastest_transactions_margin_left">
-            {sliceData5(from)}
+          <div className="lastest_transactions_p lastest_transactions_font_small">
+            <span style={{ marginRight: 16 }}></span>
+            {calcTime(timestamp)}
           </div>
         </div>
-        <div className="lastest_transactions_cell_content">
-          <div className="lastest_transactions_font_small">To</div>
-          <div className="lastest_transactions_font_big lastest_transactions_margin_left">
-            {sliceData5(to)}
+
+        <div className="lastest_transactions_cell">
+          <div className="lastest_transactions_cell_content">
+            <div className="lastest_transactions_font_small">From</div>
+            <div className="lastest_transactions_font_big lastest_transactions_margin_left">
+              {sliceData5(from)}
+            </div>
+          </div>
+          <div className="lastest_transactions_cell_content">
+            <div className="lastest_transactions_font_small">To</div>
+            <div className="lastest_transactions_font_big lastest_transactions_margin_left">
+              {sliceData5(to)}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="lastest_transactions_cell">
-        <div
-          className="lastest_transactions_cell_content lastest_transactions_font_small"
-          style={{ flexDirection: 'column', gap: 0, alignItems: 'flex-start' }}
-        >
-          {wrapString(type)}
+        <div className="lastest_transactions_cell">
+          <div
+            className="lastest_transactions_cell_content lastest_transactions_font_small"
+            style={{
+              flexDirection: 'column',
+              gap: 0,
+              alignItems: 'flex-start',
+            }}
+          >
+            {wrapString(type)}
+          </div>
+          <div className="lastest_transactions_cell_content lastest_transactions_font_big">{`${amount?.toFixed(
+            5,
+          )} AMB`}</div>
         </div>
-        <div className="lastest_transactions_cell_content lastest_transactions_font_big">{`${amount?.toFixed(
-          5,
-        )} AMB`}</div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default LatestTransactions;
