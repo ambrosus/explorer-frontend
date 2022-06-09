24958c68a4d3062d007e32303876a7b514097c65
melnicenkovadik <melnicenkovadik@gmail.com>
2022-06-09 15:40:02 +0200

    to css file

--- src/components/Chart/Tooltip.tsx
+++ src/components/Chart/Tooltip.tsx
@@ -1,7 +1,6 @@
 import { CustomTooltipProps } from '../../pages/Home/home.interfaces';
 import React from 'react';
 
-//TODO delete file?
 export const CustomTooltip = ({
   payload,
   label,
@@ -10,23 +9,7 @@ export const CustomTooltip = ({
   if (active) {
     return (
       <div
-        className="chart_custom_tooltip"
-        //TODO to css file
-        style={{
-          background: '#FFFFFF',
-          border: '1px solid #F4F5F6',
-          opacity: 0.96,
-          filter: 'drop-shadow(0px 4px 12px rgba(74, 56, 174, 0.15))',
-          padding: '10px',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'flex-start',
-          borderRadius: '2px',
-          width: '200px',
-          fontSize: '14px',
-          color: '#333',
-        }}
+        className="chart_custom_tooltip custom-style"
       >
         <p className="chart_intro">{label}</p>
         <p className="chart_label">
