94375ea8b308625b17e1666c860cbfc1789aa13c
melnicenkovadik <melnicenkovadik@gmail.com>
2022-06-13 21:42:55 +0200

    atlasFix

--- src/services/atlas.service.ts
+++ src/services/atlas.service.ts
@@ -1,11 +1,15 @@
 import API from '../API/api';
 
-export const getAtlasData = async (sortTerm: any, next: any) => {
+export const getAtlasesData = async (sortTerm: any, next: any) => {
   const data: any = await API.getAtlases({
     limit: 20,
     sort: sortTerm,
     next: next,
   });
+  return data;
+};
+export const getAtlasData = async (address: string,) => {
+  const data: any = await API.getAtlas(address)
   return data;
 };
 
