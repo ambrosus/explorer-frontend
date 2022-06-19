db02fb650c1c50e8bca28be758da1de6df04465b
melnicenkovadik <melnicenkovadik@gmail.com>
2022-06-20 01:47:10 +0200

    fixes

--- src/components/BlocksContent/blocks-content.test.tsx
+++ src/components/BlocksContent/blocks-content.test.tsx
@@ -1,27 +1,36 @@
-
 import BlocksContent from "./index";
 import {render} from "@testing-library/react";
 import {ResultHomePageData} from "../../pages/Home/home.interfaces";
-const globalAny: any = global;
-globalAny.window = {};
-globalAny.document = {};
-//fix error: ● Test suite failed to run
-//     ReferenceError: global is not defined
-//     at Object.window.document.createElement (
+
 describe("BlocksContent", () => {
   it("renders correctly", () => {
-    const data :ResultHomePageData = {
+    const data: ResultHomePageData = {
       latestBlocks: [
         {
           number: 1,
           timestamp: 213124124,
           miner: "0x0000000000000000000000000000000000000000",
           totalTransactions: 0,
-          blockRewards: 0,
           key: 21,
           index: 0,
           validator: "0x0000000000000000000000000000000000000000",
-          blockReward: 12,
+          blockRewards: [
+            {
+              reward: {
+                ether: 212312312312
+              }
+            },
+            {
+              reward: {
+                ether: 212312312312
+              }
+            },
+            {
+              reward: {
+                ether: 212312312312
+              }
+            }
+          ],
           name: "name",
         },
       ],
@@ -41,7 +50,7 @@ describe("BlocksContent", () => {
         },
       ],
     }
-    const { container } = render(<BlocksContent data={data} />);
+    const {container} = render(<BlocksContent data={data}/>);
     expect(container).toMatchSnapshot();
   });
 });
