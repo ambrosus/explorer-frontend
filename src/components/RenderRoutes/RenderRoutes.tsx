aec3081b3de9aeccec751f10242ed12ef18ca89a
melnicenkovadik <melnicenkovadik@gmail.com>
2022-06-21 15:32:47 +0200

    update

--- src/components/RenderRoutes/RenderRoutes.tsx
+++ src/components/RenderRoutes/RenderRoutes.tsx
@@ -74,12 +74,19 @@ export const RenderRoutes = (props: any) => {
           {...route}
         />
       ))}
+      {routes.blockRoutes.map((route: any) => (
+        <Route
+          suspense={<Loader />}
+          key={route.key}
+          path={route.path}
+          element={<route.component />}
+          {...route}
+        />
+      ))}
       <Route path="*" element={<Error404 />} />
       <Route path="/notfound" element={<Error404 />} />
     </Routes>
   ) : (
     <Loader />
   );
-  // Hello Comit
-  // kakogo xy9 eto tyt delaet?)
 };
