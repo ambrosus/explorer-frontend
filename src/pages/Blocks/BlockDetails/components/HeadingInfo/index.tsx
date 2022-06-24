517173be3f692f99749226f871d67c6f923865b0
melnicenkovadik <melnicenkovadik@gmail.com>
2022-06-24 11:28:56 +0200

    block details add

--- src/pages/Blocks/BlockDetails/components/HeadingInfo/index.tsx
+++ src/pages/Blocks/BlockDetails/components/HeadingInfo/index.tsx
@@ -2,19 +2,13 @@ import React, { FC } from 'react';
 
 const HeadingInfo = ({ address = '', block = { miner: '' } }: any) => (
   <div className="block_main_title">
-    <div className='block_main_title__in'>
-      <h1 className="block_main_title_heading">
-        Block details
-      </h1>
+    <div className="block_main_title__in">
+      <h1 className="block_main_title_heading">Block details</h1>
       <span className="block_main_title_heading_block">{address}</span>
     </div>
-    <div className='block_main_title__in'>
-      <div className="block_main_title_validator">
-        Validator{' '}
-      </div>
-      <span className="block_main_title_address">
-          {block?.miner ?? ''}
-        </span>
+    <div className="block_main_title__in">
+      <div className="block_main_title_validator">Validator </div>
+      <span className="block_main_title_address">{block?.miner ?? ''}</span>
     </div>
   </div>
 );
