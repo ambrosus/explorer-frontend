3f58e6e74cfe27de6d80d4b94436d7b5baf0fbba
melnicenkovadik <melnicenkovadik@gmail.com>
2022-06-27 14:35:30 +0200

    fixes

--- src/pages/Blocks/index.tsx
+++ src/pages/Blocks/index.tsx
@@ -1,50 +1,54 @@
 import useSortData from '../../hooks/useSortData';
-import { getBlocksData } from '../../services/block.service';
+import {getBlocksData} from '../../services/block.service';
 import BlocksBody from './components/BlocksBody';
 import BlocksHeader from './components/BlocksHeader';
 import DataTitle from './components/DataTitle';
 import MainInfoBlocks from './components/MainInfoBlocks';
-import { Content } from 'components/Content';
+import {Content} from 'components/Content';
 import Loader from 'components/Loader';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useLocation} from "react-router";
 
 export const Blocks = () => {
-  const { ref, renderData, loading } = useSortData(
+  const history = useLocation();
+  const {ref, renderData, loading} = useSortData(
     getBlocksData,
     null,
     'totalBundles',
   );
-
+  useEffect(() => {
+    console.log('history',history);
+  }, [history])
   return (
     <Content>
       <Content.Header>
         <div className="block_main_title">
           <h1 className="main_info_blocks_heading">Blocks</h1>
         </div>
-        <MainInfoBlocks />
+        <MainInfoBlocks/>
       </Content.Header>
       <Content.Body>
         <div className="blocks_main">
-          <DataTitle title="Blocks" />
+          <DataTitle title="Blocks"/>
           <div className="blocks_main_table">
-            <BlocksHeader />
+            <BlocksHeader/>
             {renderData && renderData.data && renderData.data.length
               ? renderData.data.map((item: any, index: number) => (
-                  <BlocksBody
-                    index={index}
-                    lastCardRef={
-                      renderData.data.length - 1 === index &&
-                      renderData?.pagination?.hasNext
-                        ? ref
-                        : undefined
-                    }
-                    key={index}
-                    item={item}
-                  />
-                ))
+                <BlocksBody
+                  index={index}
+                  lastCardRef={
+                    renderData.data.length - 1 === index &&
+                    renderData?.pagination?.hasNext
+                      ? ref
+                      : undefined
+                  }
+                  key={index}
+                  item={item}
+                />
+              ))
               : null}
           </div>
-          {!loading && renderData?.pagination?.hasNext && <Loader />}
+          {!loading && renderData?.pagination?.hasNext && <Loader/>}
         </div>
       </Content.Body>
     </Content>
