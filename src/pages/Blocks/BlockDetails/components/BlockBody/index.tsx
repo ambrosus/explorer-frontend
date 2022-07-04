c4bb563389bcbed3616bbf364d99ff502e2659a9
melnicenkovadik <melnicenkovadik@gmail.com>
2022-07-04 10:25:26 +0200

    update

--- src/pages/Blocks/BlockDetails/components/BlockBody/index.tsx
+++ src/pages/Blocks/BlockDetails/components/BlockBody/index.tsx
@@ -1,26 +1,35 @@
 import Amb from '../../../../../assets/icons/Cryptos/Amb';
-import { Number } from 'components/Number';
+import {Number} from 'components/Number';
 import moment from 'moment';
 import React from 'react';
-import { sliceData5 } from 'utils/helpers';
+import {sliceData5} from 'utils/helpers';
+import {NavLink} from 'react-router-dom';
 
-const BlockBody = ({ lastCardRef, item }: any) => {
-  const { type, blockHash, from, to, timestamp, value } = item;
+const BlockBody = ({lastCardRef, item}: any) => {
+  const {type, blockHash, from, to, timestamp, value} = item;
   const amount = value?.ether || 0;
   return (
     <div className="block_body" ref={lastCardRef}>
       <div className="block_body_cell color-gray">{sliceData5(blockHash)}</div>
       <div className="block_body_cell">{type}</div>
-      <div className="block_body_cell color-gray">{sliceData5(from)} </div>
-      <div className="block_body_cell color-gray">{sliceData5(to)}</div>
+      <div className="block_body_cell color-gray">
+        <NavLink className="universall_light2" to={`/addresses/${from}`}>
+          {sliceData5(from)}
+        </NavLink>
+      </div>
+      <div className="block_body_cell color-gray">
+        <NavLink className="universall_light2" to={`/addresses/${from}`}>
+          {sliceData5(to)}
+        </NavLink>
+      </div>
       <div className="block_body_cell">
         {moment(timestamp * 1000).fromNow()}
       </div>
       <div className="block_body_cell">
         <span className="block_body_cell_icon">
-          <Amb /> <span className="color-gray">AMB</span>
+          <Amb/> <span className="color-gray">AMB</span>
         </span>
-        <Number value={amount} fixed={8} />
+        <Number value={amount} fixed={8}/>
       </div>
     </div>
   );
