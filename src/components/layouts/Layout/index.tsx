43d851ddea275933dda1a9bc4714f650dc8b6899
dimavoronin <zveruha1109>
2022-09-08 11:01:38 +0300

    added npm menu

--- src/components/layouts/Layout/index.tsx
+++ src/components/layouts/Layout/index.tsx
@@ -1,5 +1,8 @@
-import { Header } from '../Header';
+import useAuthorization from '../../../hooks/useAuthorization';
 import { NewHeader } from '../NewHeader';
+import { useWeb3React } from '@web3-react/core';
+// @ts-ignore
+import Menu from 'airdao-menu/build';
 import React, { FC } from 'react';
 
 export interface LayoutProps {
@@ -10,10 +13,20 @@ export interface LayoutProps {
 @param {React.ReactNode} children
 @return {React.FC<LayoutProps>}
  */
-export const Layout: FC<LayoutProps> = ({ children }) => (
-  <div className="layout ">
-    <Header />
-    <NewHeader />
-    <div className="page">{children}</div>
-  </div>
-);
+export const Layout: FC<LayoutProps> = ({ children }) => {
+  const { account: address } = useWeb3React();
+  const { loginMetamask, logout } = useAuthorization();
+
+  return (
+    <div className="layout ">
+      <Menu
+        address={address}
+        login={loginMetamask}
+        logout={logout}
+        initHidden
+      />
+      <NewHeader />
+      <div className="page">{children}</div>
+    </div>
+  );
+};
