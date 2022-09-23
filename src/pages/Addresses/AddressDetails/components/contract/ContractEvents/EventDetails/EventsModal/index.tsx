38771836ca00a909d2d63ec0e36a56819239d096
Georg Grigoryan <antikiok@gmail.com>
2022-09-23 15:38:50 +0200

    fix

--- src/pages/Addresses/AddressDetails/components/contract/ContractEvents/EventDetails/EventsModal/index.tsx
+++ src/pages/Addresses/AddressDetails/components/contract/ContractEvents/EventDetails/EventsModal/index.tsx
@@ -3,9 +3,11 @@ import React, { memo } from 'react';
 const EventModal = ({ type, name, value, indexed }: any) => {
   return (
     <div className="contract_events-body-modal-cell">
-      <div>{`${type} ${name}`}</div>
+      <div
+        style={{ display: 'flex', alignItems: 'center' }}
+      >{`${type} ${name}`}</div>
       <div className="contract_events-body-modal-address">
-        {indexed ? value : value.toString()}
+        {value.toString()}
       </div>
     </div>
   );
