04bfb799015b2674fd995c1ee2d6c49c0446d5ac
d.voronin <echelon1109@gmail.com>
2023-08-16 13:45:25 +0300

    removed console log

--- src/pages/Addresses/AddressDetails/components/contract/ContractDetails/index.tsx
+++ src/pages/Addresses/AddressDetails/components/contract/ContractDetails/index.tsx
@@ -13,7 +13,6 @@ import { useQuery } from 'react-query';
 
 const ContractDetails = (props: any) => {
   const { contractInfo, address, updateContract } = props;
-  console.log(address);
   const [selectedTab, selectTab] = useState('verify');
 
   useEffect(() => {
