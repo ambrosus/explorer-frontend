7bfe28b869322fb8f553fe26f75fb698a1a6e357
d.voronin <echelon1109@gmail.com>
2023-10-12 12:43:11 +0300

    added menu

--- src/components/layouts/Layout/index.tsx
+++ src/components/layouts/Layout/index.tsx
@@ -1,8 +1,14 @@
+// @ts-nocheck
 import { NewHeader } from '../NewHeader';
+import { Header } from '@airdao/ui-library';
 import { useWeb3React } from '@web3-react/core';
-// @ts-ignore
-import { Menu } from 'airdao-components-and-tools/components';
-import React, { FC } from 'react';
+import { useAuthorization, useAutoLogin } from 'airdao-components-and-tools/hooks';
+import {
+  metamaskConnector,
+  walletconnectConnector,
+} from 'airdao-components-and-tools/utils';
+import React, { FC, useEffect, useState } from 'react';
+import {ethers} from "ethers";
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -12,13 +18,39 @@ export interface LayoutProps {
 @param {React.ReactNode} children
 @return {React.FC<LayoutProps>}
  */
+
 export const Layout: FC<LayoutProps> = ({ children }) => {
-  const web3ReactInstance = useWeb3React();
+  const [balance, setBalance] = useState('0');
+  const { account, provider } = useWeb3React();
+
+  const { loginMetamask, loginWalletConnect, logout } = useAuthorization(
+    metamaskConnector,
+    walletconnectConnector,
+  );
+  const isLoaded = useAutoLogin(metamaskConnector);
+
+  useEffect(() => {
+    getBalance();
+  }, [account]);
+
+  const getBalance = async () => {
+    if (!provider) return;
+
+    const bnBalance = await provider.getBalance(account);
+    const numBalance = ethers.utils.formatEther(bnBalance);
+    setBalance((+numBalance).toFixed(2));
+  };
 
   return (
     <div className="layout ">
       <div className="container" style={{ position: 'relative' }}>
-        <Menu web3ReactInstance={web3ReactInstance} initHidden />
+        <Header
+          loginMetamask={loginMetamask}
+          loginWalletConnect={loginWalletConnect}
+          account={account}
+          disconnect={logout}
+          balance={balance}
+        />
       </div>
       <NewHeader />
       <div className="page">{children}</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,30 @@
+// @ts-nocheck
 import Main from './components/Main/Main';
 import { store } from './state';
 import { Web3ReactProvider } from '@web3-react/core';
-import { providers } from 'ethers';
+import {
+  metamaskConnector,
+  metamaskHooks,
+  walletconnectConnector,
+  walletconnectHooks,
+} from 'airdao-components-and-tools/utils';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-/*
- * @param {Provider} store - redux store
- * @param {BrowserRouter} BrowserRouter - react router
- * @param {Main} Main - main component
- */
-const queryClient = new QueryClient();
+const connectors = [
+  [metamaskConnector, metamaskHooks],
+  [walletconnectConnector, walletconnectHooks],
+];
 
-const getLibrary = (provider: any = null) =>
-  new providers.Web3Provider(provider);
+const queryClient = new QueryClient();
 
 export const App = (): JSX.Element => (
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3ReactProvider connectors={connectors}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Main />
         </BrowserRouter>
