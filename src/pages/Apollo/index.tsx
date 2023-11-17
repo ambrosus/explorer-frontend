01f1cc12e1d90b383cb3f6592fbf5405f2146fbe
d.voronin <echelon1109@gmail.com>
2023-11-17 23:28:39 +0200

    add retired node check

--- src/pages/Apollo/index.tsx
+++ src/pages/Apollo/index.tsx
@@ -2,16 +2,9 @@ import API2 from '../../API/newApi';
 import AtlasBlocksHeader from '../Atlas/components/AtlasBlocksHeader';
 import TabsNew from '../Transactions/components/TabsNew';
 import ApolloBlocksBody from './components/ApolloBlocksBody';
-import {
-  AmbErrorProvider,
-  Contracts,
-  ContractNames,
-} from '@airdao/airdao-node-contracts';
-// @ts-ignore
-import { getCurrentAmbNetwork } from 'airdao-components-and-tools/utils';
+import { getRetiredApollos } from './utils';
 import { Content } from 'components/Content';
 import HeadInfo from 'components/HeadInfo';
-import { utils } from 'ethers';
 import { useTypedSelector } from 'hooks/useTypedSelector';
 import React, { memo, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
@@ -57,51 +50,6 @@ export const Apollo = memo(() => {
   useEffect(() => {
     getRetiredApollos();
   }, []);
-
-  const timestampToFormattedDate = (timestamp: number) => {
-    const date = new Date(timestamp * 1000); // Умножаем на 1000, так как Unix-время измеряется в миллисекундах
-
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
-    const year = date.getUTCFullYear();
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-
-    return `${day}.${month}.${year} ${hours}:${minutes} UTC`;
-  };
-
-  const getRetiredApollos = async () => {
-    const network = getCurrentAmbNetwork();
-    const provider = new AmbErrorProvider(network.rpcUrl, network.chainId);
-    // @ts-ignore
-    const contracts = new Contracts(provider, network.chainId);
-
-    const lockKeeper = contracts.getContractByName(ContractNames.LockKeeper);
-    const serverNodes = contracts.getContractByName(
-      ContractNames.ServerNodesManager,
-    );
-    const arr = [];
-
-    const locks = await lockKeeper.getAllLocks();
-    for (const lock of locks) {
-      if (lock.locker !== serverNodes.address) continue;
-      if (!lock.description.startsWith('ServerNodes unstake: ')) continue;
-
-      const address = lock.description.slice('ServerNodes unstake: '.length);
-      const nodeInfo = await serverNodes.stakes(address);
-      if (!nodeInfo.stake.isZero()) continue;
-
-      arr.push({
-        address: lock.description.replace('ServerNodes unstake: ', '0x'),
-        isRetired: true,
-        unlockTime: timestampToFormattedDate(
-          +utils.formatUnits(lock.firstUnlockTime, 0),
-        ),
-        amount: utils.formatEther(lock.intervalAmount),
-      });
-    }
-    return arr;
-  };
 
   return (
     <Content>
--- src/pages/Apollo/utils/index.tsx
+++ src/pages/Apollo/utils/index.tsx
@@ -1,3 +1,12 @@
+// @ts-ignore
+import {
+  AmbErrorProvider,
+  ContractNames,
+  Contracts,
+} from '@airdao/airdao-node-contracts';
+// @ts-ignore
+import { getCurrentAmbNetwork } from 'airdao-components-and-tools/utils';
+import { utils } from 'ethers';
 import React from 'react';
 
 export const getApollosNetworkInfo = (data: any) => {
@@ -13,4 +22,49 @@ export const getApollosNetworkInfo = (data: any) => {
   }
 
   return { total, online, offline, connecting };
+};
+
+const timestampToFormattedDate = (timestamp: number) => {
+  const date = new Date(timestamp * 1000); // Умножаем на 1000, так как Unix-время измеряется в миллисекундах
+
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
+  const year = date.getUTCFullYear();
+  const hours = String(date.getUTCHours()).padStart(2, '0');
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+
+  return `${day}.${month}.${year} ${hours}:${minutes} UTC`;
+};
+
+export const getRetiredApollos = async () => {
+  const network = getCurrentAmbNetwork();
+  const provider = new AmbErrorProvider(network.rpcUrl, network.chainId);
+  // @ts-ignore
+  const contracts = new Contracts(provider, network.chainId);
+
+  const lockKeeper = contracts.getContractByName(ContractNames.LockKeeper);
+  const serverNodes = contracts.getContractByName(
+    ContractNames.ServerNodesManager,
+  );
+  const arr = [];
+
+  const locks = await lockKeeper.getAllLocks();
+  for (const lock of locks) {
+    if (lock.locker !== serverNodes.address) continue;
+    if (!lock.description.startsWith('ServerNodes unstake: ')) continue;
+
+    const address = lock.description.slice('ServerNodes unstake: '.length);
+    const nodeInfo = await serverNodes.stakes(address);
+    if (!nodeInfo.stake.isZero()) continue;
+
+    arr.push({
+      address: lock.description.replace('ServerNodes unstake: ', '0x'),
+      isRetired: true,
+      unlockTime: timestampToFormattedDate(
+        +utils.formatUnits(lock.firstUnlockTime, 0),
+      ),
+      amount: utils.formatEther(lock.intervalAmount),
+    });
+  }
+  return arr;
 };
